b41bdaef8761face8b8a1b62cd4c624bea0c2b5b
HugoRochartk <hugorochartk@gmail.com>
2023-06-13 00:37:16 +0100

    comecei jogo

--- game.py
+++ game.py
@@ -1,3 +1,6 @@
+from random import choice
+from time import sleep
+
 def create_empty_map():
 	map_ = [
 			[' ', ' ', ' '], 
@@ -9,4 +12,10 @@ def create_empty_map():
 	
 
 def start_game(player1, player2):
-	print(f"\ncomeçar entre {player1} e {player2}.")
+	print(f'\nIniciando o jogo entre {player1} e {player2}...')
+	sleep(2)
+	print(f'\nGerando aleatoriamente o primeiro a jogar...')
+	first_player = choice([player1, player2])
+	sleep(2)
+	print(f'\nO primeiro jogador a jogar é {first_player}.')
+	map_ = create_empty_map()
